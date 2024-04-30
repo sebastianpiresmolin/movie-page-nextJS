68be6574979ddd54ed2c94894fa809554e45999a
sebastianpiresmolin <piresmolinsecure@gmail.com>
2024-04-30 17:38:22 +0200

    added cookie storage to authContext

--- app/contexts/authContext.tsx
+++ app/contexts/authContext.tsx
@@ -1,6 +1,6 @@
-// authContext.tsx
 'use client';
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext, useEffect } from 'react';
+import Cookies from 'js-cookie';
 
 interface IAuthContext {
   isLoggedIn: boolean;
@@ -14,8 +14,20 @@ const AuthContext = createContext<IAuthContext>({
 
 export const useAuth = () => useContext(AuthContext);
 
-export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const [isLoggedIn, setLoggedIn] = useState(false);
+export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [isLoggedIn, setLoggedIn] = useState(() => {
+    return !!Cookies.get('isLoggedIn');
+  });
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      Cookies.set('isLoggedIn', 'true');
+    } else {
+      Cookies.remove('isLoggedIn');
+    }
+  }, [isLoggedIn]);
 
   return (
     <AuthContext.Provider value={{ isLoggedIn, setLoggedIn }}>
--- middleware.ts
+++ middleware.ts
@@ -13,12 +13,6 @@ export function middleware(request: NextRequest) {
   // Get the token from the cookies
   const token = request.cookies.get('token')?.value || '';
 
-  // Redirect logic based on the path and token presence
-  if (isPublicPath && token) {
-    // If trying to access a public path with a token, redirect to the home page
-    return NextResponse.redirect(new URL('/', request.nextUrl));
-  }
-
   // If trying to access a protected path without a token, redirect to the login page
   if (!isPublicPath && !token) {
     return NextResponse.redirect(new URL('/login', request.nextUrl));
