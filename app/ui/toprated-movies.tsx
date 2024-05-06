5c09bfb525c386cdbdf6d85268bdcb02c963e07f
Rasmus Bäckström <rasmusbaeckstrom@gmail.com>
2024-05-06 16:22:57 +0200

    Added type, href and some adjustments

--- app/ui/toprated-movies.tsx
+++ app/ui/toprated-movies.tsx
@@ -1,28 +1,35 @@
-import React from "react";
-import { getTopRatedMovies } from "../lib/data";
+import React from 'react';
+import Link from 'next/link';
+import { getTopRatedMovies } from '../lib/data';
+
+interface Movie {
+    _id: string;
+    title: string;
+    image: string;
+}
+
+type MovieList = Movie[];
 
 export async function TopRatedMovies() {
-    const movies = await getTopRatedMovies();
+    const movies: MovieList = await getTopRatedMovies();
 
     return (
-        <div>
+        <div className="container mx-auto py-8">
             <h1 className="text-xl text-black font-bold mb-4 text-center">Popular right now</h1>
-            <ul className="flex flex-wrap gap-4">
-                {movies.map((movie: any) => (
-                    <li key={movie._id} className="w-0/3">
-                        <div className="bg-gray-50 shadow-md p-4 rounded-lg">
+            <div className="flex overflow-x-auto space-x-4 lg:justify-center">
+                {movies.map((movie: Movie) => (
+                    <Link href={`/movies/${movie._id}`} key={movie._id}>
+                        <div className="min-w-[150px] md:min-w-[200px] lg:max-w-[300px] bg-gray-50 shadow-md p-4 rounded-lg hover:shadow-lg transition-shadow duration-300">
                             <h2 className="text-lg text-black font-semibold mb-2 text-center">{movie.title}</h2>
                             <img
                                 src={movie.image}
                                 alt={movie.title}
-                                className="w-40 h-auto rounded-md"
+                                className="w-full h-auto rounded-md"
                             />
                         </div>
-                    </li>
+                    </Link>
                 ))}
-            </ul>
+            </div>
         </div>
     );
 }
-
-
