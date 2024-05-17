35cf1f9a85682f15706ba91e05a88b6a1f2ec100
Willi86 <williamlundqvist@yahoo.com>
2024-05-17 13:25:04 +0200

    Changed the styling  of movie card to follow graphic profile
    bg-TXGPink +  balck text(story)
    +tailwind.config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,7 +13,11 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
           'background': "url('/images/background.png')",
+        
       },
+      colors: {
+        TXGPink: '#BA837B',
+    },
       keyframes: {
         upDownSize: {
           '0%, 100%': {
