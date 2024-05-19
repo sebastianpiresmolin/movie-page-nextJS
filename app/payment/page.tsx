bd9be0a033f4e47e210405fedd63ea606c44b020
Willi86 <williamlundqvist@yahoo.com>
2024-05-20 00:14:40 +0200

    Booking- seats-salon-dates-Unfinished
    This code is unfinished, The payment page still not done(right now it just text on the page).
    The testing is only on movie number 1 (toystory) the other movies does not have the database fileds .

---
+++ app/payment/page.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+const PaymentPage: React.FC = () => {
+  return (
+    <div className="h-full w-full max-w-screen flex-grow min-h-screen
+    p-8">
+      <h2 className="text-2xl font-bold  text-red-900">Payment</h2>
+      <p className='mt-2 text-2xl font-bold text-red-900'>Proceed with your payment here</p>
+      
+    </div>
+  );
+};
+
+export default PaymentPage;
